31c42c472e3975669860609cbf3bbc68c0b3e63a
Rago2002forever1 <you@example.com>
2024-02-13 12:00:04 +0530

    ssss

--- src/Display/Imagecomp.jsx
+++ src/Display/Imagecomp.jsx
@@ -11,12 +11,12 @@ import nine from "../assets/Sai/nine.jpg"
 import ten from "../assets/Sai/ten.jpg"
 import "./imagecomp.css"
 
-export default function Imagecomps() {
+export default function Imagecomp() {
   return (
     <div style={{display:"flex", justifyContent:"center",alignItems:"center",flexDirection:"column"}}>
       <h1>different souls</h1>
 
-      <img src={one} alt='Mountain' />
+      <img src={one} alt="Mountain "/>
       <p>"Never measure the height of a mountain until you reach the top.Then you see how low it  was"</p>
 
       <img src={two} alt="ocean" />
